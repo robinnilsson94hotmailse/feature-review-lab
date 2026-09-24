b694e4de6d258b4042defd4106fdabeaf18a0552
Robin Nilsson <robin.nilsson94@hotmail.se>
2026-09-24 15:03:35 +0200

    Add personalized welcome message

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Frågar efter namnet
+Console.WriteLine("Vad heter du?");
+//Läser användarens input
+string namn = Console.ReadLine();
+
+//Kontrollerar om input är tom elelr bara mellanslag
+if (string.IsNullOrWhiteSpace(namn))
+{
+    //Om input är ogiltig
+    Console.WriteLine("Du måste skriva ett namn!");
+}
+else
+{
+    //Om namnet är giltigt
+    Console.WriteLine($"Välkommen {namn}.");
+}
